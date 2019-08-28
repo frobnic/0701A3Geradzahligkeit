f015141fc3f3fbed6db22241ba0576917b5ff9e3
frank paulsen <frobnic+ita@gmail.com>
2019-08-28 11:54:25 +0200

    inserted code

--- 0701A3Geradzahligkeit/Program.cs
+++ 0701A3Geradzahligkeit/Program.cs
@@ -8,8 +8,18 @@ namespace _0701A3Geradzahligkeit
 {
     class Program
     {
+        static Boolean AmIStraight(int a)
+        {
+            return (a % 2 == 0);
+        }
+
         static void Main(string[] args)
         {
+            int b = 0;
+
+            Console.Write("Bitte erfassen sie eine Zahlin: ");
+            if (int.TryParse(Console.ReadLine(), out b))
+                Console.WriteLine("Die Geradzahligkeitsprüfung der Zahlin ergibt: {0}", AmIStraight(b));
         }
     }
 }
